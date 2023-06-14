2804fc04ffed7a66a98db09aea108fb24976b852
Daniele <88.danrossetti@gmail.com>
2023-06-14 20:20:55 +0200

    giorno 6 - js su logo nav

--- script.js
+++ script.js
@@ -4,6 +4,23 @@ const searchbox = document.querySelector('.searchbox');
 const clear = document.querySelector('.searchbox__input-clear');
 const searchboxText = document.getElementById('searchbox__input-text');
 
+const nav = document.querySelector('nav');
+const navPos = nav.getBoundingClientRect().top;
+
+
+
+
+window.addEventListener("scroll", e => {
+    let viewportHeigth = window.innerHeight;
+    let scrollPos = window.scrollY;
+
+    if (scrollPos > navPos){
+        nav.classList.add("stick");
+    } else {
+        nav.classList.remove("stick");
+    }
+});
+
 icon.onclick = () => {
     if (searchboxText.value === ""){
         searchbox.classList.toggle('active');
